8b75fbb324a164daee3f6e3fae355eef2849dd48
KarinaF29 <fran.lilypm@gmail.com>
2025-06-10 12:03:32 -0300

    Update listaexercicio3_Karina.py

--- listaexercicio3_Karina.py
+++ listaexercicio3_Karina.py
@@ -29,8 +29,8 @@ st.header("Lista Exercício 3 Karina")
 
 import pandas as pd
 
-arquivo = "/content/projetos.csv"
-df = pd.read_csv("/content/projetos.csv", sep=";")
+arquivo = "projetos.csv"
+df = pd.read_csv(arquivo, sep=';')
 st.dataframe(df.head(len(df)))
 
 """2) Atualize a DataFrame criado no Exe1, adicionando mais uma linha ao final com os dados referentes ao mês de dezembro de 2026. Depois apresente as últimas linhas da df para checar se ocorreu como deveria. (Peso: 2,0)
